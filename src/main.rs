2a1990b935189095007ead9595663c7ba6a95a0a
r.andronache <netlemon2021@gmail.com>
2022-07-08 11:49:51 +0100

    Added naive implementation

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,56 @@
-fn main() {}
+use std::cell::UnsafeCell;
+use std::sync::atomic::{ AtomicBool, Ordering };
+
+const LOCKED: bool = true;
+const UNLOCKED: bool = false;
+
+pub struct Mutex<T> {
+    locked: AtomicBool,
+    v: UnsafeCell<T>
+}
+
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
+impl<T> Mutex<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            locked: AtomicBool::new(UNLOCKED),
+            v: UnsafeCell::new(t)
+        }
+    } 
+
+    pub fn with_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        while self.locked.load(Ordering::Relaxed) != UNLOCKED {}
+
+        /**
+         * /\ LOAD
+         * .. threads can data race in between
+         * \/ STORE
+         */
+        self.locked.store(LOCKED, Ordering::Relaxed);
+        let res = f(unsafe { &mut *self.v.get() });
+        self.locked.store(UNLOCKED, Ordering::Relaxed);
+
+        res
+    }
+}
+
+fn main() {
+    let l: &'static _ = Box::leak(Box::new(Mutex::new(0)));
+    let handles: Vec<_> = (0..10)
+        .map(|_| {
+            std::thread::spawn(move || {
+                for _ in 0..100 {
+                    l.with_lock(|v| {
+                        *v += 1;
+                    });
+                }
+            })
+        }).collect();
+    
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    assert_eq!(l.with_lock(|v| *v), 10 * 100);
+}
